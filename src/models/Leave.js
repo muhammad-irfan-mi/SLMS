fb8547704260f53cdc790aeacf76f524824d9641
muhammad-irfan-mi <irfanmalikhu@gmail.com>
2025-11-26 18:05:26 +0500

    change

--- src/models/Leave.js
+++ src/models/Leave.js
@@ -4,12 +4,12 @@ const leaveSchema = new mongoose.Schema(
   {
     school: { type: mongoose.Schema.Types.ObjectId, ref: "School", required: true },
 
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
     studentName: String,
     classId: { type: mongoose.Schema.Types.ObjectId, ref: "ClassSection", required: true },
     sectionId: { type: mongoose.Schema.Types.ObjectId, required: true },
 
-    teacherId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    teacherId: { type: mongoose.Schema.Types.ObjectId, ref: "User"},
     teacherName: String,
 
     userType: { type: String, enum: ["student", "teacher"], default: "student" },
