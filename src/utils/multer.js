57d3b2aa03aad1bf8e5ba350414cc791f8d33829
muhammad-irfan-mi <irfanmalikhu@gmail.com>
2026-03-06 16:02:18 +0500

    change file size

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -21,7 +21,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 20 * 1024 * 1024 },
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 module.exports = {
