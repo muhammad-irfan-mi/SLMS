8937c5a6d290ec1fd6a540e822ef63f8461f460b
muhammad-irfan-mi <irfanmalikhu@gmail.com>
2026-05-14 12:02:51 +0500

    chage in service route

--- src/routes/servicePermission.routes.js
+++ src/routes/servicePermission.routes.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { protect, isSuperAdmin } = require('../middlewares/auth');
-const {
-  createService,
-  getAllServices,
-  updateService,
-  deleteService,
-  getServiceById
-} = require('../controllers/servicePermission.controller');
+const { createService, updateService, getServiceById, deleteService, getAllServices } = require('../controllers/servicePermission.controller');
+
 
 
 router.post('/',protect, isSuperAdmin, createService);
 router.put('/:id', protect, isSuperAdmin, updateService);
 router.get('/:id', protect, isSuperAdmin, getServiceById);
 router.delete('/:id', protect, isSuperAdmin, deleteService);
-router.get('/', getAllServices);
+router.get('/', protect, isSuperAdmin, getAllServices);
 
 module.exports = router;
