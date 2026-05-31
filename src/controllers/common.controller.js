a610caabdc16884943376151ceb85de741ee1288
muhammad-irfan-mi <irfanmalikhu@gmail.com>
2026-05-31 23:15:28 +0500

    change

--- src/controllers/common.controller.js
+++ src/controllers/common.controller.js
@@ -1032,6 +1032,7 @@ const login = async (req, res, Model, userType) => {
             id: user._id,
             name: user.name,
             email: user.email,
+            permissions: user.permissions || [],
             role: user.role,
             school: user.school
         };
