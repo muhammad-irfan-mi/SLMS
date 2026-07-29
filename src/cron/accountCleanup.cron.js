bdd78cbb185ce449c760a1a420945b2cfcc8d64c
muhammad-irfan-mi <irfanmalikhu@gmail.com>
2026-07-29 16:20:34 +0500

    update cron

--- src/cron/accountCleanup.cron.js
+++ src/cron/accountCleanup.cron.js
@@ -1,63 +1,3 @@
-// const cron = require("node-cron");
-// const Student = require("../models/Student");
-
-// const startStudentCron = async () => {
-
-//     cron.schedule("* * * * *", async () => {
-
-//         try {
-            
-//             console.log("Running Student Cleanup Cron...");
-            
-//             const sevenDaysAgo = new Date(
-//                 Date.now() - (7 * 24 * 60 * 60 * 1000)
-//             );
-
-//             const result = await Student.updateMany(
-//                 {
-//                     isActive: false,
-//                     status: "active",
-//                     isRestorable: true,
-//                     deactivatedAt: {
-//                         $lte: sevenDaysAgo
-//                     }
-//                 },
-//                 {
-//                     $set: {
-//                         status: "left",
-//                         isRestorable: false
-//                     }
-//                 }
-//             );
-
-//             console.log(
-//                 `${result.modifiedCount} student(s) marked as LEFT`
-//             );
-
-//         } catch (err) {
-
-//             console.error(
-//                 "Student Cron Error:",
-//                 err.message
-//             );
-
-//         }
-
-//     });
-
-// };
-
-// module.exports = startStudentCron;
-
-
-
-
-
-
-
-
-
-
 const cron = require("node-cron");
 const Student = require("../models/Student");
 const Staff = require("../models/Staff");
@@ -93,7 +33,6 @@ const startAccountCleanupCron = () => {
 
     console.log("✅ Account cleanup job scheduled.");
 
-    // Production
     cron.schedule("0 0 * * *", async () => {
 
         console.log("[CRON] Running Account Cleanup...");
